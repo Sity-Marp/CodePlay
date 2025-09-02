a14638e02cc333444624616603f4542b1824f3fb
Bisma <bisma@DESKTOP-D74DC0C>
2025-09-02 15:45:25 +0200

    test

--- frontend/pages/login.jsx
+++ frontend/pages/login.jsx
@@ -1,11 +1,12 @@
-import Footer from '@/components/Footer'
+import Footer from "@/components/Footer";
 
-function login (){
-    return (
-        <div>
-        <h1>Login</h1>
-        <Footer />
-        </div>
-    )
+function login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <Footer />
+    </div>
+  );
+  //test
 }
 export default login();
