29a7cb61c13efb984a7d13417da17fdbb2e90416
Jessicabii3 <159479108+Jessicabii3@users.noreply.github.com>
2025-09-08 15:47:18 +0200

    La till models

---
+++ Backend/Models/AnswerOption.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend.Models
+{
+    public class AnswerOption
+    {
+        public int Id { get; set; }                      // Primärnyckel
+        public string Text { get; set; } = "";           // Text som visas i UI
+        public bool IsCorrect { get; set; }              // TRUE om detta är rätt svar
+
+       
+        public int QuestionId { get; set; }              // FK
+        public Question? Question { get; set; }
+    }
+}
---
+++ Backend/Models/Question.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+namespace Backend.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }                     // Primärnyckel
+        public string Text { get; set; } = "";          // Själva frågetexten som visas i UI
+
+        // FK till Quiz 
+        public int QuizId { get; set; }                 // FK
+        public Quiz? Quiz { get; set; }                 // Navigation till sitt quiz
+
+        
+        public int Order { get; set; } = 1;             // Visningsordning
+
+       
+        public List<AnswerOption> AnswerOptions { get; set; } = new();
+    }
+}
