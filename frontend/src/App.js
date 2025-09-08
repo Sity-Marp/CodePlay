e9e9c3d1ddb68fb54ed3267da3fecbae8ecb5f6d
mmdnn <mmdnn@M>
2025-09-08 10:16:13 +0200

    login, register, reset password

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Background from "./components/Background";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ResetPassword from "./pages/ResetPassword";
 import "./App.css";
 
 function App() {
@@ -10,12 +13,17 @@ function App() {
     <BrowserRouter>
       <Background />
       <Navbar />
+      
       <Routes>
         <Route path="/" element={<Home />} />{" "}
         {/* Startsida - utloggad användare */}
         <Route path="/login" element={<Login />} /> 
+        <Route path="/register" element={<Register />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
+    
   );
 }
 
